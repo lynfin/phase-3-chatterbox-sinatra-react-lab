b127c5e44768e98cdd7e38a83d2f9c6ef75d95a4
Lyn <lynfinman@gmail.com>
2022-07-10 15:25:31 -0500

    completed lab

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,34 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add routes
-  
+  get '/messages' do
+    # get all messages
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  get '/messages/:id' do
+    message = Message.find(params[:id])
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(body: params[:body], username: params[:username])
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
 end
